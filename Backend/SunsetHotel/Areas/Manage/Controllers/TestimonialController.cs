90272d69e77cd9dcece360e06479e7e11951d9ac
Kanan172411 <kanangg@code.edu.az>
2021-10-17 20:16:54 +0400

    Testimonial Crud

---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/TestimonialController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Helpers;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class TestimonialController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        public TestimonialController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index(int page=1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.Testimonials.Count() / 6d);
+            if (page> ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<Testimonial> testimonials = _context.Testimonials.Skip((page - 1) * 6).Take(6).ToList();
+            return View(testimonials);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Testimonial testimonial)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (testimonial.ImageFile != null)
+            {
+                if (testimonial.ImageFile.ContentType != "image/jpeg" && testimonial.ImageFile.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("ImageFile", "Fayl   .jpg ve ya   .png ola biler!");
+                    return View();
+                }
+
+                if (testimonial.ImageFile.Length > 3145728)
+                {
+                    ModelState.AddModelError("ImageFile", "Fayl olcusu 3mb-dan boyuk ola bilmez!");
+                    return View();
+                }
+
+                testimonial.ImageName = FileManager.Save(_env.WebRootPath, "assets/img", testimonial.ImageFile);
+            }
+            else
+            {
+                ModelState.AddModelError("ImageFile", "Image yuklemek mecburidir!");
+                return View();
+            }
+
+            _context.Testimonials.Add(testimonial);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Testimonial testimonial = _context.Testimonials.FirstOrDefault(x => x.Id == id);
+
+            if (testimonial == null) return RedirectToAction("error", "dashboard");
+
+            return View(testimonial);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Testimonial testimonial)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Testimonial existTestimonial = _context.Testimonials.FirstOrDefault(x => x.Id == testimonial.Id);
+
+            if (existTestimonial == null) return RedirectToAction("error", "dashboard");
+
+            existTestimonial.FullName = testimonial.FullName;
+            existTestimonial.Profession = testimonial.Profession;
+            existTestimonial.Desc = testimonial.Desc;
+            if (testimonial.ImageFile != null)
+            {
+                if (testimonial.ImageFile.ContentType != "image/jpeg" && testimonial.ImageFile.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("ImageFile", "Fayl   .jpg ve ya   .png ola biler!");
+                    return View();
+                }
+
+                if (testimonial.ImageFile.Length > 3145728)
+                {
+                    ModelState.AddModelError("ImageFile", "Fayl olcusu 3mb-dan boyuk ola bilmez!");
+                    return View();
+                }
+
+
+                string newFileName = FileManager.Save(_env.WebRootPath, "assets/img", testimonial.ImageFile);
+
+                if (!string.IsNullOrWhiteSpace(existTestimonial.ImageName))
+                {
+                    string oldFilePath = Path.Combine(_env.WebRootPath, "assets/img", existTestimonial.ImageName);
+                    if (System.IO.File.Exists(oldFilePath))
+                    {
+                        System.IO.File.Delete(oldFilePath);
+                    }
+                }
+
+
+                existTestimonial.ImageName = newFileName;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Testimonial existTestimonial = _context.Testimonials.FirstOrDefault(x => x.Id == id);
+            if (existTestimonial == null)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+
+            if (!string.IsNullOrWhiteSpace(existTestimonial.ImageName))
+            {
+                FileManager.Delete(_env.WebRootPath, "assets/img", existTestimonial.ImageName);
+            }
+            _context.Testimonials.Remove(existTestimonial);
+            _context.SaveChanges();
+            return Json(new { status = 200 });
+        }
+    }
+}
--- Backend/SunsetHotel/Models/Testimonial.cs
+++ Backend/SunsetHotel/Models/Testimonial.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,5 +22,7 @@ namespace SunsetHotel.Models
         public string Desc { get; set; }
         [StringLength(maximumLength:500)]
         public string ImageName { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
     }
 }
