57c196a3738db945dedf0db28de34c68949aeb3f
Kanan172411 <kanangg@code.edu.az>
2021-10-17 20:53:19 +0400

    Gallery Crud

---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/GalleryController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Helpers;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class GalleryController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        public GalleryController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index(int page=1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.Galleries.Count() / 8d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<Gallery> galleries = _context.Galleries.Skip((page - 1) * 8).Take(8).ToList();
+            return View(galleries);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Gallery gallery)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (gallery.ImageFile != null)
+            {
+                if (gallery.ImageFile.ContentType != "image/jpeg" && gallery.ImageFile.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("ImageFile", "Fayl   .jpg ve ya   .png ola biler!");
+                    return View();
+                }
+
+                if (gallery.ImageFile.Length > 3145728)
+                {
+                    ModelState.AddModelError("ImageFile", "Fayl olcusu 3mb-dan boyuk ola bilmez!");
+                    return View();
+                }
+
+                gallery.ImageName = FileManager.Save(_env.WebRootPath, "assets/img", gallery.ImageFile);
+            }
+            else
+            {
+                ModelState.AddModelError("ImageFile", "Image yuklemek mecburidir!");
+                return View();
+            }
+
+            _context.Galleries.Add(gallery);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Gallery existGallery = _context.Galleries.FirstOrDefault(x => x.Id == id);
+            if (existGallery == null)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+
+            if (!string.IsNullOrWhiteSpace(existGallery.ImageName))
+            {
+                FileManager.Delete(_env.WebRootPath, "assets/img", existGallery.ImageName);
+            }
+            _context.Galleries.Remove(existGallery);
+            _context.SaveChanges();
+            return Json(new { status = 200 });
+        }
+    }
+}
--- Backend/SunsetHotel/Models/Gallery.cs
+++ Backend/SunsetHotel/Models/Gallery.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +13,7 @@ namespace SunsetHotel.Models
         public int Id { get; set; }
         [StringLength(maximumLength:70)]
         public string ImageName { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
     }
 }
