54c45119d6dda7e738a27617566b11fa5879f3ea
Kanan172411 <kanangg@code.edu.az>
2021-10-18 02:52:19 +0400

    Header Footer Data Crud

--- Backend/SunsetHotel/Areas/Manage/Controllers/HeaderFooterDatasController.cs
+++ Backend/SunsetHotel/Areas/Manage/Controllers/HeaderFooterDatasController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,45 @@ using System.Threading.Tasks;
 
 namespace SunsetHotel.Areas.Manage.Controllers
 {
+    [Area("Manage")]
     public class HeaderFooterDatasController : Controller
     {
+        private readonly AppDbContext _context;
+        public HeaderFooterDatasController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            HeaderFooterData headerFooterData = _context.HeaderFooterDatas.FirstOrDefault();
+            return View(headerFooterData);
+        }
+        public IActionResult Edit(int id)
+        {
+            HeaderFooterData headerFooterData = _context.HeaderFooterDatas.FirstOrDefault(x=>x.Id==id);
+
+            if (headerFooterData == null) return RedirectToAction("error", "dashboard");
+
+            return View(headerFooterData);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(HeaderFooterData headerFooterData)
+        {
+            HeaderFooterData existData = _context.HeaderFooterDatas.FirstOrDefault(x => x.Id == headerFooterData.Id);
+
+            if (existData == null) return RedirectToAction("error", "dashboard");
+            existData.Location = headerFooterData.Location;
+            existData.LogoPart1 = headerFooterData.LogoPart1;
+            existData.LogoPart2 = headerFooterData.LogoPart2;
+            existData.Number = headerFooterData.Number;
+            existData.Google = headerFooterData.Google;
+            existData.Twitter = headerFooterData.Twitter;
+            existData.Facebook = headerFooterData.Facebook;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
         }
     }
 }
