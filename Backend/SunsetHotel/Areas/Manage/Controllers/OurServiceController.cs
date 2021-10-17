fb4ee422bd5f8b07ba1537b814d538be560265a5
Kanan172411 <kanangg@code.edu.az>
2021-10-18 01:25:07 +0400

    Tags and OurService Crud

---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/OurServiceController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class OurServiceController : Controller
+    {
+        private readonly AppDbContext _context;
+        public OurServiceController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page=1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.OurServices.Count() / 6d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<OurService> ourServices = _context.OurServices.Skip((page - 1) * 6).Take(6).ToList();
+            return View(ourServices);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(OurService ourService)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.OurServices.Add(ourService);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int id)
+        {
+            OurService ourService = _context.OurServices.FirstOrDefault(x => x.Id == id);
+
+            if (ourService == null) return RedirectToAction("error", "dashboard");
+
+            return View(ourService);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(OurService ourService)
+        {
+            OurService existOurService = _context.OurServices.FirstOrDefault(x => x.Id == ourService.Id);
+
+            if (existOurService == null) return RedirectToAction("error", "dashboard");
+
+            existOurService.Name = ourService.Name;
+            existOurService.Desc = ourService.Desc;
+            existOurService.Icon = ourService.Icon;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            OurService existOurService = _context.OurServices.FirstOrDefault(x => x.Id == id);
+            if (existOurService == null)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            _context.OurServices.Remove(existOurService);
+            _context.SaveChanges();
+            return Json(new { status = 200 });
+        }
+    }
+}
---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/TagController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+        public TagController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.Tags.Count() / 6d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<Tag> tags = _context.Tags.Skip((page - 1) * 6).Take(6).ToList();
+            return View(tags);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Tag tag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Tags.Add(tag);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Tag tag = _context.Tags.FirstOrDefault(x => x.Id == id);
+
+            if (tag == null) return RedirectToAction("error", "dashboard");
+
+            return View(tag);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Tag tag)
+        {
+            Tag existTag = _context.Tags.FirstOrDefault(x => x.Id == tag.Id);
+
+            if (existTag == null) return RedirectToAction("error", "dashboard");
+
+            existTag.Name = tag.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Tag existTag = _context.Tags.FirstOrDefault(x => x.Id == id);
+            if (existTag == null)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            _context.Tags.Remove(existTag);
+            _context.SaveChanges();
+            return Json(new { status = 200 });
+        }
+    }
+}
