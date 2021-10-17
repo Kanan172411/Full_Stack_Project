8ba9c45d2e0b0a79a60ceb9f09feea717a14c535
Kanan172411 <kanangg@code.edu.az>
2021-10-18 01:58:09 +0400

    Room Category Crud

---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/FeatureController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class FeatureController : Controller
+    {
+        private readonly AppDbContext _context;
+        public FeatureController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.Features.Count() / 6d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<Feature> features = _context.Features.Skip((page - 1) * 6).Take(6).ToList();
+            return View(features);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Feature feature)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Features.Add(feature);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Feature feature = _context.Features.FirstOrDefault(x => x.Id == id);
+
+            if (feature == null) return RedirectToAction("error", "dashboard");
+
+            return View(feature);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Feature feature)
+        {
+            Feature existFeature = _context.Features.FirstOrDefault(x => x.Id == feature.Id);
+
+            if (existFeature == null) return RedirectToAction("error", "dashboard");
+
+            existFeature.Name = feature.Name;
+            existFeature.Desc = feature.Desc;
+            existFeature.StarCount = feature.StarCount;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            Feature existFeature = _context.Features.FirstOrDefault(x => x.Id == id);
+            if (existFeature == null)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            _context.Features.Remove(existFeature);
+            _context.SaveChanges();
+            return Json(new { status = 200 });
+        }
+    }
+}
---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/RoomCategoryController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class RoomCategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public RoomCategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.RoomCategories.Count() / 6d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<RoomCategory> roomCategories = _context.RoomCategories.Skip((page - 1) * 6).Take(6).ToList();
+            return View(roomCategories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(RoomCategory roomCategories)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.RoomCategories.Add(roomCategories);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Edit(int id)
+        {
+            RoomCategory roomCategory = _context.RoomCategories.FirstOrDefault(x => x.Id == id);
+
+            if (roomCategory == null) return RedirectToAction("error", "dashboard");
+
+            return View(roomCategory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(RoomCategory roomCategory)
+        {
+            RoomCategory existRoomCategory = _context.RoomCategories.FirstOrDefault(x => x.Id == roomCategory.Id);
+
+            if (existRoomCategory == null) return RedirectToAction("error", "dashboard");
+
+            existRoomCategory.Name = roomCategory.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+            RoomCategory existRoomCategory = _context.RoomCategories.FirstOrDefault(x => x.Id == id);
+            if (existRoomCategory == null)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            _context.RoomCategories.Remove(existRoomCategory);
+            _context.SaveChanges();
+            return Json(new { status = 200 });
+        }
+    }
+}
