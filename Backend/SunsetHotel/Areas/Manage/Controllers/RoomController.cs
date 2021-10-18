e6e424065c1f8b312d560f60245e6bda8d6747f2
Kanan172411 <kanangg@code.edu.az>
2021-10-18 07:19:23 +0400

    Room Crud index

---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/RoomController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class RoomController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        public RoomController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.Rooms.Count() / 6d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<Room> rooms = _context.Rooms.Include(x => x.RoomImages).Include(x=>x.Categories).Skip((page - 1) * 6).Take(6).ToList();
+            return View(rooms);
+        }
+    }
+}
