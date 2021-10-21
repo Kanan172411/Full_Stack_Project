2415637cf8b05a69baf47d58d8a3f8f54e51ff7a
Kanan172411 <kanangg@code.edu.az>
2021-10-21 05:15:28 +0400

    Subscribers Admin side

---
+++ Backend/SunsetHotel/Areas/Manage/Controllers/SubscriberController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SunsetHotel.DAL;
+using SunsetHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunsetHotel.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    [Authorize(Roles = "SuperAdmin, Admin, Editor")]
+    public class SubscriberController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SubscriberController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page = 1)
+        {
+            ViewBag.SelectedPage = page;
+            ViewBag.TotalPage = Math.Ceiling(_context.Tags.Count() / 10d);
+            if (page > ViewBag.TotalPage)
+            {
+                return RedirectToAction("error", "dashboard");
+            }
+            List<Subscriber> subscribers = _context.Subscribers.Skip((page - 1) * 10).Take(10).ToList();
+            return View(subscribers);
+        }
+    }
+}
