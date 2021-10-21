e808ef16666a463d8f425274c51c60bfd743cd4f
Kanan172411 <kanangg@code.edu.az>
2021-10-21 21:53:03 +0400

    Blog Filter

--- Backend/SunsetHotel/Controllers/BlogController.cs
+++ Backend/SunsetHotel/Controllers/BlogController.cs
@@ -18,21 +18,45 @@ namespace SunsetHotel.Controllers
         {
             _context = context;
         }
-        public IActionResult Index(int page=1)
+        public IActionResult Index(int? categoryId, int? Tagid, string search, int page=1)
         {
             TempData["forSelect"] = 4;
-            double pageCount = Math.Ceiling(_context.Blogs.Count() / 3d);
-            if (page>pageCount)
+
+            var query = _context.Blogs.Include(x=>x.BlogCategory).Include(x=>x.BlogTags).ThenInclude(x=>x.Tag).AsQueryable();
+            var queryBlogTag = _context.BlogTags.AsQueryable();
+            if (categoryId != null)
+            {
+                query=query.Where(x => x.BlogCategoryId == categoryId);
+                ViewBag.categoryId = categoryId;
+            }
+            if (Tagid != null)
+            {
+                var queryNew = queryBlogTag.Where(x => x.TagId == Tagid).ToList();
+                List<Blog> blogs1 = new List<Blog>();
+                foreach (var item in queryNew)
+                {
+                    blogs1.Add(_context.Blogs.Find(item.BlogId));
+                }
+                query = blogs1.AsQueryable();
+                ViewBag.tagId = Tagid;
+            }
+            if (search != null)
+            {
+                ViewBag.search = search;
+                query = query.Where(x => x.Name.ToLower().Contains(search.ToLower()));
+            }
+            double pageCount = Math.Ceiling(query.Count() / 3d);
+            if (page > pageCount)
             {
                 return RedirectToAction("error", "home");
             }
+            Setting setting = _context.Settings.FirstOrDefault();
             ViewBag.TotalPage = pageCount;
             ViewBag.SelectedPage = page;
-            Setting setting = _context.Settings.FirstOrDefault();
             ViewBag.WelcomeContent = setting.BlogWelcomeContent;
             BlogViewModel blogVM = new BlogViewModel
             {
-                blogs = _context.Blogs.Include(x => x.BlogCategory).Include(x => x.BlogTags).Skip((page - 1) * 3).Take(3).ToList(),
+                blogs = query.Skip((page - 1) * 3).Take(3).ToList(),
                 tags = _context.Tags.ToList(),
                 categories = _context.BlogCategories.ToList(),
                 recentPost = _context.Blogs.OrderByDescending(x=>x.Createdat).Take(4).ToList()
