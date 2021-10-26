4f0d769f1d5b0db93a2ac81f91074d8515818d15
Kanan172411 <kanangg@code.edu.az>
2021-10-26 23:21:43 +0400

    Some Reportings Added

--- Backend/SunsetHotel/Areas/Manage/Controllers/DashBoardController.cs
+++ Backend/SunsetHotel/Areas/Manage/Controllers/DashBoardController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SunsetHotel.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +13,29 @@ namespace SunsetHotel.Areas.Manage.Controllers
     [Authorize(Roles = "SuperAdmin, Admin, Editor")]
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            int monthly = 0;
+            foreach (var item in _context.Reservations.Include(x => x.room).Where(x => x.Status == true && x.CheckIn >= DateTime.Now.AddMonths(-1) && x.CheckOut <= DateTime.Now))
+            {
+                monthly += item.NightCount * item.room.Price;
+            }
+            ViewBag.monthly = monthly;
+            int annual = 0;
+            foreach (var item in _context.Reservations.Include(x => x.room).Where(x => x.Status == true && x.CheckIn >= DateTime.Now.AddYears(-1) && x.CheckOut <= DateTime.Now))
+            {
+                annual += item.NightCount * item.room.Price;
+            }
+            ViewBag.Annual = annual;
+            double allBooking = _context.Reservations.Count();
+            double acceptedBooking = _context.Reservations.Where(x => x.Status == true).Count();
+            ViewBag.AcceptedPercent = (int)acceptedBooking * 100 / (int)allBooking;
+            ViewBag.Pending = _context.Reservations.Where(x => x.Status == null).Count();
             return View();
         }
         public IActionResult Error()
